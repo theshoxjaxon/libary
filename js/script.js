5d119ef29431aeed8488a0ac9dbd4852f1ea1346
shoxjaxon7 <shox652xolmurodov@gmail.com>
2022-02-12 00:13:35 +0500

    done new function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 "use strict";
+const search = "ielts";
 let elHeader = document.querySelector(".header__div");
 let elLogoutBtn = document.querySelector(".logout");
+let elList = document.querySelector(".main__list");
 let inputBooks = document.querySelector(".input");
 let elRuslt = document.querySelector(".result");
+let elTemplate = document.querySelector(".template");
 
 let lokalToken = window.localStorage.getItem("token");
 if (!lokalToken) {
@@ -22,4 +25,65 @@ let request = fetch(
   "https://www.googleapis.com/books/v1/volumes?q=search+ielts"
 )
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => data);
+
+// const reanderBooks = function (arr, element) {
+//   arr.forEach((element) => {
+//     const clonedBookTemplate = elTemplate.cloneNode(true);
+//     clonedBookTemplate.querySelector(".book__img").src = element;
+//   });
+//   console.log(element);
+// };
+// reanderBooks();
+
+const getBooks = async function () {
+  let respons = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${search}+terms`
+  );
+  let data = await respons.json();
+
+  if (data.respons === true && data.search.lenght > 1) {
+    reanderBooks(data.items, elList);
+  }
+  let arr1 = data.items;
+  // console.log(arr1);
+  arr1.forEach((element) => {
+    let newItem = document.createElement("li");
+    newItem.classList.add("main__items");
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("dfdsfads");
+    newItem.appendChild(newDiv);
+    let newImg = document.createElement("img");
+    newImg.classList.add("book__img");
+    newImg.setAttribute("width", 201);
+    newImg.setAttribute("height", 202);
+    newDiv.appendChild(newImg);
+    let newHead = document.createElement("h4");
+    let newArr = arr1.slice();
+    newItem.appendChild(newHead);
+    console.log(newArr);
+    newHead.textContent = newArr.id;
+    // console.log(element);
+    // newHead.textContent = arr1.volumeInfo.
+    elList.appendChild(newItem);
+  });
+  // let html = `<li class="main__items">
+  //             <div class="dfdsfads">
+  //               <img class="book__img" src="./images/item.png" alt="" width="201" height="202" />
+  //             </div>
+  //             <h4>Python</h4>
+  //             <div class="item__descs">
+  //               <p>David M. Beazley</p>
+  //               <p>2009</p>
+  //             </div>
+  //             <div class="item__buttons d-flex">
+  //               <button class="button button__warning">Bookmark</button>
+  //               <button class="button button__info">More Info</button>
+  //             </div>
+  //             <button class="button button__secondary w-100">Read</button>
+  //           </li>`;
+  // elList.insertAdjacentHTML("beforeend", html);
+
+  // console.log(data);
+};
+getBooks();
