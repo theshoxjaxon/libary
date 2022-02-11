f96854cda2f1c1a4c91d1b7c887f45c2e6dafdb4
shoxjaxon7 <shox652xolmurodov@gmail.com>
2022-02-11 13:40:36 +0500

    ad new home.html

---
+++ js/login.js
@@ -0,0 +1,28 @@
+"use strict";
+let elForm = document.querySelector(".form");
+let elInputName = document.querySelector(".input__name");
+let elInputPassword = document.querySelector(".input__password");
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  let nameValue = elInputName.value;
+  let passwordValue = elInputPassword.value;
+  fetch("https://reqres.in/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: "eve.holt@reqres.in",
+      password: "cityslicka",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data?.token) {
+        window.localStorage.setItem("token", data.token);
+
+        window.location.replace("home.html");
+      }
+    });
+});
