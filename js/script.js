ca6a8aa97bd426b8e45649854c81e0a13758a78d
shoxjaxon7 <shox652xolmurodov@gmail.com>
2022-02-11 16:54:53 +0500

    fall out

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
+let elHeader = document.querySelector(".header__div");
 let elLogoutBtn = document.querySelector(".logout");
+let inputBooks = document.querySelector(".input");
+let elRuslt = document.querySelector(".result");
+
 let lokalToken = window.localStorage.getItem("token");
 if (!lokalToken) {
   window.location.replace(index.html);
@@ -8,3 +12,14 @@ elLogoutBtn.addEventListener("click", function () {
   window.localStorage.removeItem("token");
   window.location.replace("index.html");
 });
+
+elHeader.addEventListener("click", function (event) {
+  event.preventDefault();
+  let booksValue = inputBooks.value;
+  inputBooks.innerHTML = "";
+});
+let request = fetch(
+  "https://www.googleapis.com/books/v1/volumes?q=search+ielts"
+)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
